05cd076e8f40ebf4222c7ea1801f4973bbabdfca
MadSweeney46 <jan.walewski@hotmail.de>
2020-08-01 23:25:24 +0200

    Created a script for dual polybar  launch

--- polybar/launchpolybar.sh
+++ polybar/launchpolybar.sh
@@ -7,6 +7,14 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar mainbar &
+i=0
 
-echo "Polybar launched..."
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	##WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}')
+	MONITOR=$m polybar bar$i &
+	echo $m
+	echo $i
+	echo bar$i
+	((i=i+1))
+done
+echo "Polybars launched..."
