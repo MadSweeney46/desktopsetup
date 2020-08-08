36b58a208359888b7c66d4addbdb33d1959c3d89
MadSweeney46 <jan.walewski@hotmail.de>
2020-08-08 15:26:08 +0200

    finalized the workspace order. added a scripts folder which contains sxhkd scripts in order to add some functionality. Added a script which gets the next or previous desktop. Restructured the sxhkd file and commented some scripts out that are pretty useless for me.

---
+++ sxhkd/scripts/getNextOrPrevDesktop.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+allDesktops=($(bspc query -D --names))
+currentDesktop=$(bspc query -D -d --names)
+
+if [ $1 == "+" ]; then
+	if (( currentDesktop < ${allDesktops[${#allDesktops[@]}-1]} ));then
+		((currentDesktop=currentDesktop+1))
+	else
+		((currentDesktop=1))
+	fi
+elif [ $1 == "-" ]; then
+	if (( currentDesktop > 1 )); then
+		((currentDesktop=currentDesktop-1))	
+	else
+		((currentDesktop=${allDesktops[${#allDesktops[@]}-1]} ))
+	fi
+fi
+
+echo $currentDesktop
+#bspc node -d $(((  $(bspc query -D -d --names) {-,+} 1 ))) -f
+
+
