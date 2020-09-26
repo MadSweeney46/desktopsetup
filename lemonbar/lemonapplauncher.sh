054a6fa69ec60458b5b62c7d59ff5cafe9540366
MadSweeney46 <jan.walewski@hotmail.de>
2020-09-27 01:33:44 +0200

    The & after $1 is necessary otherwise the script gets locked and toggling isn't possible

--- lemonbar/lemonapplauncher.sh
+++ lemonbar/lemonapplauncher.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 #$1 the app to launch
-
 if [ ! -z "$1" ] ; then
   pid=$(pgrep $1)
-  echo $pid
   if [ -z $pid ] ; then
-    echo "jwa launch process"
-    $1
+    $1 &
   else
-    echo "jwa kill process"
     kill -SIGTERM $pid
   fi
 else
